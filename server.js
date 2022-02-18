c38a7b127d156125c40371a8237e9c7fd991e0af
faizanshah14 <faizanshah1496@gmail.com>
2022-02-18 21:43:15 +0500

    logging process env variables

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.use((req, res, next) => {
   next();
 });
 
+console.log('process.env.BUCKET_NAME',process.env.BUCKET_NAME);
+
+console.log(process.env);
 app.use(require('morgan')('dev'));
 app.use(require('body-parser').urlencoded({ limit: '50mb', extended: true }));
 app.use(require('body-parser').json({ limit: '50mb' }));
